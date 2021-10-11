b64fbcdf7ccadcbee7847ee5964834a10595f01d
PiotrAntolik <antolcrew@gmail.com>
2021-10-11 20:16:29 +0200

    implement insert buttons,plus and result-  js logic

--- script.js
+++ script.js
@@ -1,1 +1,42 @@
-console.log("siema");
+
+var firstNumber = 0;
+var secondNumber = 0;
+var sign = "";
+
+var inputOutput =  document.getElementById("input");
+
+function add(number)
+{
+     
+            if(inputOutput.innerHTML == 0)
+            {
+                  inputOutput.innerHTML = number; 
+            }
+            else
+            {
+                  inputOutput.innerHTML = inputOutput.innerHTML + number;
+            }       
+}
+
+function sum()
+{
+      
+      firstNumber =  inputOutput.innerHTML;
+      inputOutput.innerHTML = 0;
+      sign = "plus";
+}
+
+function result()
+{
+      secondNumber = inputOutput.innerHTML;
+      firstNumber = parseInt(firstNumber);
+      secondNumber = parseInt(secondNumber);
+     
+      switch(sign)
+      {
+            case "plus":
+            inputOutput.innerHTML = firstNumber + secondNumber;
+            break;
+      }
+}
+
