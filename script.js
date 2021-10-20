d9e21a351049953c4f5fbd22f771a3a6ef1190d7
PiotrAntolik <antolcrew@gmail.com>
2021-10-20 17:08:16 +0200

    finished js logic to all buttons

--- script.js
+++ script.js
@@ -1,42 +1,136 @@
-
 var firstNumber = 0;
 var secondNumber = 0;
 var sign = "";
+var mark;
+var markTask;
+var inputOutput =  document.getElementById("resultHooker");
+var taskOutput = document.getElementById("taskHooker");
+var number = inputOutput.innerHTML;
 
-var inputOutput =  document.getElementById("input");
 
 function add(number)
 {
-     
-            if(inputOutput.innerHTML == 0)
+      inputOutput.innerHTML = inputOutput.innerHTML + number;
+      
+            if(inputOutput.innerHTML == 0+number)
             {
                   inputOutput.innerHTML = number; 
+            
             }
-            else
-            {
-                  inputOutput.innerHTML = inputOutput.innerHTML + number;
-            }       
+      if(mark)
+      {
+            inputOutput.innerHTML = number; 
+            mark = false;
+      }
+
+      if(markTask)
+      {
+            taskOutput.innerHTML = "";
+            markTask = false;
+      }
+               
 }
 
-function sum()
+function operation(action)
 {
       
       firstNumber =  inputOutput.innerHTML;
-      inputOutput.innerHTML = 0;
-      sign = "plus";
+      taskOutput.innerHTML = firstNumber + " " + action + " ";
+      sign = action;
+      mark = true;
+      console.log(sign);
 }
 
 function result()
 {
+      
       secondNumber = inputOutput.innerHTML;
-      firstNumber = parseInt(firstNumber);
-      secondNumber = parseInt(secondNumber);
+      firstNumber = parseFloat(firstNumber);
+      secondNumber = parseFloat(secondNumber);
      
       switch(sign)
       {
-            case "plus":
+            case "+":
             inputOutput.innerHTML = firstNumber + secondNumber;
             break;
+
+            case "-":
+            inputOutput.innerHTML = firstNumber - secondNumber;
+            break;
+
+            case "/":
+            inputOutput.innerHTML = firstNumber / secondNumber;
+            break;
+
+            case "*":
+            inputOutput.innerHTML = firstNumber * secondNumber;
+            break;
+
+            
       }
+      taskOutput.innerHTML = firstNumber + " " + sign + " " + secondNumber + " =";
+      mark = true;
+      markTask = true;
 }
+
+
+function clearInput()
+{
+      inputOutput.innerHTML = 0;
+      taskOutput.innerHTML = null;
+      firstNumber = 0;
+      secondNumber = 0;
+      sign = "";
+     
+}
+
+function clearEnter()
+{
+      inputOutput.innerHTML = 0;
+}
+
+function deleteLastChar()
+{
+     
+     var text =  inputOutput.innerHTML;
+     if(text.length == 1)
+     {
+           inputOutput.innerHTML = "0";     
+     }
+     else
+     {
+      inputOutput.innerHTML = text.substring(0, text.length -1 );
+     }
+  
+     
+    
+}
+
+function mirror()
+{
+    var number = inputOutput.innerHTML;
+    var result = number/-1;
+    inputOutput.innerHTML = result;
+
+}
+
+function dot()
+{
+    
+      var number = inputOutput.innerHTML;
+    
+      if(!number.includes("."))
+      {
+            var result = number+".";
+            inputOutput.innerHTML = result;
+      }
+      
+      
+
+      
+          
+
+      
+}
+
 
